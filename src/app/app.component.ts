b0eabace5a77297c1fb5c37dc468a215402c0343
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-04-22 15:40:03 -0300

    ingresoTotal function created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { Ingreso } from './ingreso/ingreso.model';
+import { Egreso } from './egreso/egreso.model';
+import { IngresoServicio } from './ingreso/ingreso.service';
+import { EgresoServicio } from './egreso/egreso.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'presupuesto-app';
+  ingresos: Ingreso[] = [];
+  egresos: Egreso[] = [];
+
+  constructor(
+    private ingresoServicio: IngresoServicio,
+    private egresoServicio: EgresoServicio
+  ) {
+    this.ingresos = ingresoServicio.ingresos;
+    this.egresos = egresoServicio.egresos;
+  }
+
+  getIngresoTotal() {
+    let ingresoTotal: number = 0;
+    this.ingresos.forEach((ingreso) => {
+      ingresoTotal = +ingreso.valor;
+    });
+    return ingresoTotal;
+  }
 }
